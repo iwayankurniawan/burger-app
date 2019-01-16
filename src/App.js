6b419c05f0b7674f206873aff67e55e4321329f9
iwayankurniawan <kurniawanadityawardana@gmail.com>
2019-01-16 15:46:19 +0100

    Set Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
+import { modelInstance } from './StoreData/DataModel';
+import { Redirect } from 'react-router';
+import { Route } from 'react-router-dom';
+
+import HeaderProjectView from './View/HeaderBarView';
+import MainView from './View/MainView';
+import SideBarView from './View/SideBarView';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div className="App container-fluid row">
+        <div className="header-bar row">
+          <HeaderProjectView model={modelInstance}/>
+        </div>
+        <div className="row">
+          <div className="sidebar col-sm-4">
+            <SideBarView model={modelInstance}/>
+          </div>
+          <div className="main-content col-sm-8">
+            <Route path="/"  render={() => <MainView model={modelInstance}/>}/>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/StoreData/DataModel.js
@@ -0,0 +1,17 @@
+const dataModel = function () {
+  var observers = [];
+  //--------- Observer pattern
+  this.addObserver = function (observer) {
+    observers.push(observer);
+  };
+
+  this.removeObserver = function (observer) {
+    observers = observers.filter(o => o !== observer);
+  };
+
+  const notifyObservers = function (obj) {
+    observers.forEach(o => o.update(obj));
+  };
+};
+
+export const modelInstance = new dataModel();
