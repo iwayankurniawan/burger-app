bc5dbd34a31c012efd96202be11df2bd89c81e06
frozzi18 <fahrurrozzi18@gmail.com>
2019-01-24 14:33:18 +0100

    Edit Layout Karir Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { modelInstance } from './StoreData/DataModel';
 import { Redirect } from 'react-router';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import HeaderProjectView from './View/HeaderBarView';
 import MainView from './View/MainView';
@@ -25,7 +25,14 @@ class App extends Component {
         </div>
         <div className="row">
           <div className="main-content col-sm-12">
-            <Route path="/"  render={() => <MainView model={modelInstance}/>}/>
+            <Switch>
+              <Route path="/menu"  render={() => <MenuView model={modelInstance}/>}/>
+              <Route path="/outlet"  render={() => <LocationView model={modelInstance}/>}/>
+              <Route path="/kontak"  render={() => <ContactView model={modelInstance}/>}/>
+              <Route path="/story"  render={() => <StoryView model={modelInstance}/>}/>            
+              <Route path="/karir"  render={() => <KarirView model={modelInstance}/>}/>   
+              <Route path="/"  render={() => <MainView model={modelInstance}/>}/>          
+            </Switch>
           </div>
         </div>
         <div className="row">
--- src/View/KarirView.js
+++ src/View/KarirView.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router';
 
+import './KarirView.css';
+
 class KarirView extends Component {
   constructor(props) {
     super(props)
@@ -28,8 +30,27 @@ class KarirView extends Component {
 
   render() {
     return(
-      <div className="row">
-        <p>Karir</p>
+      <div>
+        <div className="row karir-background">
+          <p>Karir</p>
+        </div>
+        <div className="col-sm-6 karir-background-white">
+          <p>Kabobs ingin menjadi tempat bekerja yang menyenangkan untuk seluruh Crew Kabobs. Supaya selalu bisa memberikan pelayanan terbaik untuk setiap pelanggan dimanapun mereka berada.</p>
+          <h1>Posisi yang tersedia</h1>
+          <h2>OUTLET CREW DI OUTLET BIP LT. 3</h2>
+          <h2>Marketing</h2>
+          <div className="row">
+            <button type="button" class="btn btn-outline-secondary btn-lg">Apply</button>
+          </div>
+        </div>
+        <div className="col-sm-6 karir-background-white">
+          <p>Oulet Dago Lt. 2 Jl. Ir. H. Djuanda No. 310, Kota Bandung 40135</p>
+          <p>email</p>
+          <p>line</p>
+          <p>instagram</p>
+          <p>Dapatkan email tentang promo, menu baru dan lowongan pekerjaan.</p>
+          <p>Insert Email</p>
+        </div>
       </div>
     );
   }
